93fcdd3e6cd86b69438fa3b790bc9c5a11023402
Woojong Kim <woojong92@gmail.com>
2018-09-14 16:19:20 +0900

    queue

---
+++ 1158_조세퍼스문제(queue).cpp
@@ -0,0 +1,28 @@
+/********************************************************
+	조세퍼스 문제
+	문제출처- https://www.acmicpc.net/problem/1158
+********************************************************/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	queue<int> q;
+	for(int i=1; i<=n; i++){
+		q.push(i);
+	}
+	printf("<");
+	for(int i=0; i<n-1; i++){
+		for(int j=0; j<m-1; j++){
+			q.push(q.front());
+			q.pop();
+		}
+		printf("%d, ", q.front());
+		q.pop();
+	}
+	printf("%d>\n", q.front());
+	return 0;
+} 
+
