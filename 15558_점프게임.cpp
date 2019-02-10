e0938eae2daddc8d474cc869a8031d10141ab651
Woojong Kim <woojong92@gmail.com>
2019-02-10 22:03:04 +0900

    .

--- 15558_점프게임.cpp
+++ 15558_점프게임.cpp
@@ -9,19 +9,26 @@
 #include <string>
 #include <vector>
 using namespace std;
+
 int main() {
     int n, k;
     cin >> n >> k;
     vector<string> a(2);
+
     for (int i=0; i<2; i++) {
         cin >> a[i];
     }
+
     vector<pair<int,int>> dirs = {{0,1},{0,-1},{1,k}};
     vector<vector<int>> d(2, vector<int>(n, -1));
+
     queue<pair<int,int>> q;
+
     d[0][0] = 0;
     q.push(make_pair(0,0));
+
     bool ok = false;
+
     while (!q.empty()) {
         int x, y;
         tie(x,y) = q.front(); q.pop();
@@ -43,6 +50,8 @@ int main() {
         }
         if (ok) break;
     }
+
     cout << (ok ? "1" : "0") << '\n';
+
     return 0;
 }
