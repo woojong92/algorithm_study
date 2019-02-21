f414b6389ec75492ff85a0176684d7cebd35ef09
Woojong Kim <woojong92@gmail.com>
2019-02-21 14:02:49 +0900

    new file

---
+++ 11048_이동하기_v1.cpp
@@ -0,0 +1,38 @@
+/***********************************************************
+	이동하기
+	https://www.acmicpc.net/problem/11048
+***********************************************************/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a[1001][1001];
+int d[1001][1001];
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	for( int i=1; i<=n; i++){
+		for( int j=1; j<=m; j++){
+			scanf("%d", &a[i][j]);
+		}
+	}
+	
+	for( int i=1; i<=n; i++) {
+		for( int j=1; j<=m; j++ ){
+			d[i][j] = max( max(d[i-1][j], d[i][j-1]), d[i-1][i-1]) + a[i][j];
+		}
+	}
+	
+	printf("\n");
+	for( int i=0; i<=n; i++) {
+		for( int j=0; j<=m; j++ ){
+			printf("%d ", d[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("%d\n", d[n][m]);
+	return 0;
+} 
