29b4b8c494fbb52ca590dfd6e1e3f34db8249d85
Woojong Kim <woojong92@gmail.com>
2019-03-06 22:50:34 +0900

    modified

--- 15989_123더하기4_v1.cpp
+++ 15989_123더하기4_v1.cpp
@@ -1,9 +1,27 @@
-#include <stdio.h>
+/***********************************************************
+	1,2,3 ¥ı«œ±‚ 4 
+	https://www.acmicpc.net/problem/15989
+***********************************************************/
 
+/*
+int n= 4;
+int m= 3;
+int nums= {1, 2, 3};
+int d[n];
+d[0] = 1;
+for(int j=0; j<m; j++){
+	for( int i=1; i<=n; i++) {
+		if( i-nums[j] >=0) {
+			d[i] += d[i-nums[j]];
+		}
+	}
+}
+*/
+
+#include <stdio.h>
 long long d[1000001];
 const long long mod = 10009LL;
 const int limit = 100000;
-
 int main() {
     d[0] = 1;
     for (int i=1; i<=limit; i++) {
